a293f2e8bc387b9017c532369c5adec18f6ac4f5
madina9229 <madina.kentpayeva@studio.unibo.it>
2025-04-01 15:20:36 +0200

    Implemented WebSocketHandler.scala for WebSocket connection handling.

--- rubamazzo/src/main/scala/server/WebSocketHandler.scala
+++ rubamazzo/src/main/scala/server/WebSocketHandler.scala
@@ -1,19 +1,18 @@
+package server
 
+import akka.http.scaladsl.model.ws._
+import akka.stream.scaladsl._
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+object WebSocketHandler {
+  def webSocketFlow(playerName: String): Flow[Message, Message, Any] = {
+    Flow[Message].map {
+      case TextMessage.Strict(text) =>
+        TextMessage(s"Hello $playerName! Waiting for updates...")
+      case _ =>
+        TextMessage("Unsupported message type")
+    }
+  }
+}
 
 
 
