adec86de56f70971711dad62208526fec7770988
madina9229 <madina.kentpayeva@studio.unibo.it>
2025-04-01 15:13:05 +0200

    Added Server.scala and Routes.scala for server setup and API routing.

---
+++ rubamazzo/src/main/scala/server/Routes.scala
@@ -0,0 +1,120 @@
+package server
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import server.WebSocketHandler._
+import server.Server
+import server.Server._
+import akka.http.scaladsl.model.StatusCodes
+import spray.json.DefaultJsonProtocol
+import spray.json._
+import model.GameJsonProtocol._
+import model.Game
+
+
+object Routes {
+  def gameRoutes(games: scala.collection.mutable.Map[String, Game]): Route = {
+    concat(
+      path("createGame") {
+        post {
+          val gameId = createGame()
+          complete(s"Game created with ID: $gameId")
+        }
+      },
+      path("joinGame" / Segment) { gameId =>
+        post {
+          parameter("playerName") { playerName =>
+            games.get(gameId) match {
+              case Some(game) =>
+                val updatedGame = game.copy(players = game.players :+ playerName)
+                games += (gameId -> updatedGame)
+                complete(s"Player $playerName joined game with ID: $gameId")
+              case None =>
+                complete(StatusCodes.NotFound, s"Game with ID $gameId not found")
+            }
+          }
+        }
+      },
+      path("gameState" / Segment) { gameId =>
+        get {
+          games.get(gameId) match {
+            case Some(game) =>
+              complete(game.toJson.prettyPrint)
+            case None =>
+              complete(StatusCodes.NotFound, s"Game with ID $gameId not found")
+          }
+        }
+      },
+      path("disconnectPlayer" / Segment) { gameId =>
+        post {
+          parameter("playerName") { playerName =>
+            games.get(gameId) match {
+              case Some(game) if game.players.contains(playerName) =>
+                val updatedGame = game.copy(
+                  players = game.players.filterNot(_ == playerName),
+                  disconnectedPlayers = game.disconnectedPlayers :+ playerName
+                )
+                games += (gameId -> updatedGame)
+                complete(s"Player $playerName disconnected from game with ID: $gameId")
+              case Some(_) =>
+                complete(StatusCodes.BadRequest, s"Player $playerName is not part of the game")
+              case None =>
+                complete(StatusCodes.NotFound, s"Game with ID $gameId not found")
+            }
+          }
+        }
+      },
+      path("reconnectPlayer" / Segment) { gameId =>
+        post {
+          parameter("playerName") { playerName =>
+            games.get(gameId) match {
+              case Some(game) if game.disconnectedPlayers.contains(playerName) =>
+                val updatedGame = game.copy(
+                  players = game.players :+ playerName,
+                  disconnectedPlayers = game.disconnectedPlayers.filterNot(_ == playerName)
+                )
+                games += (gameId -> updatedGame)
+                complete(s"Player $playerName reconnected to game with ID: $gameId")
+              case Some(_) =>
+                complete(StatusCodes.BadRequest, s"Player $playerName was not disconnected or does not belong to the game")
+              case None =>
+                complete(StatusCodes.NotFound, s"Game with ID $gameId not found")
+            }
+          }
+        }
+      },
+      path("makeMove" / Segment) { gameId =>
+        post {
+          parameter("playerName", "move") { (playerName, move) =>
+            games.get(gameId) match {
+              case Some(game) =>
+                if (game.players(game.currentTurn) == playerName) {
+                  // Logica per gestire la mossa qui (da implementare)
+                  updateTurn(gameId) // Aggiorna il turno dopo la mossa
+                  complete(s"Move accepted. Turn passed to the next player.")
+                } else {
+                  complete(StatusCodes.BadRequest, s"It's not $playerName's turn")
+                }
+              case None =>
+                complete(StatusCodes.NotFound, s"Game with ID $gameId not found")
+            }
+          }
+        }
+      },
+      path("currentTurn" / Segment) { gameId =>
+        get {
+          games.get(gameId) match {
+            case Some(game) =>
+              val currentPlayer = game.players(game.currentTurn)
+              complete(s"It's $currentPlayer's turn")
+            case None =>
+              complete(StatusCodes.NotFound, s"Game with ID $gameId not found")
+          }
+        }
+      },
+      path("connect" / Segment) { playerName =>
+        handleWebSocketMessages(webSocketFlow(playerName))
+      }
+    )
+
+  }
+}
---
+++ rubamazzo/src/main/scala/server/Server.scala
@@ -0,0 +1,88 @@
+package server
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.Materializer
+import spray.json._
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+import model.Game
+import model.GameJsonProtocol
+import akka.http.scaladsl.model.StatusCodes
+import spray.json.DefaultJsonProtocol
+import model.GameJsonProtocol._
+import server.Routes
+
+
+object Server extends App {
+  implicit val system: ActorSystem = ActorSystem("RubamazzoServer")
+  implicit val materializer: Materializer = Materializer(system)
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+
+  // Storage for games
+  //
+  var games: scala.collection.mutable.Map[String, Game] = scala.collection.mutable.Map()
+  val route: Route = Routes.gameRoutes(games)
+
+  val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+
+
+
+  def createGame(): String = {
+    val gameId = java.util.UUID.randomUUID().toString
+    games += (gameId -> Game(gameId, List()))
+    gameId
+  }
+
+  def updateTurn(gameId: String): Unit = {
+    games.get(gameId) match {
+      case Some(game) =>
+        val nextTurn = (game.currentTurn + 1) % game.players.size
+        val updatedGame = game.copy(currentTurn = nextTurn)
+        games += (gameId -> updatedGame)
+      case None =>
+        println(s"Game with ID $gameId not found")
+    }
+  }
+
+
+  def reconnectPlayer(gameId: String, playerName: String): String = {
+    games.get(gameId) match {
+      case Some(game) =>
+        if (game.players.contains(playerName)) {
+          s"Player $playerName reconnected to game with ID: $gameId"
+        } else {
+          "Player not part of the game"
+        }
+      case None =>
+        "Game not found"
+    }
+  }
+
+
+  def handleDisconnection(gameId: String, playerName: String): Unit = {
+    games.get(gameId) match {
+      case Some(game) =>
+        val updatedPlayers = game.players.filterNot(_ == playerName)
+        val newTurn = if (game.players(game.currentTurn) == playerName) {
+          game.currentTurn % updatedPlayers.size
+        } else {
+          game.currentTurn
+        }
+        val updatedGame = game.copy(players = updatedPlayers, currentTurn = newTurn)
+        games += (gameId -> updatedGame)
+      case None =>
+        println(s"Game with ID $gameId not found")
+    }
+  }
+
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+}
